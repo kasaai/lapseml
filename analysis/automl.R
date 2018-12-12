76cfa43bbc45c629ed64f149ad0a695527339ebf
kevinykuo <kevinykuo@gmail.com>
2018-12-12 18:37:35 -0500

    automl model reasonability check

--- analysis/automl.R
+++ analysis/automl.R
@@ -1,7 +1,13 @@
 library(h2o)
+library(recipes)
 h2o.init(nthreads = 1)
-training_h2o <- as.h2o(training_data, "training_data")
-training_h2o
+
+rec <- recipe(training_data) %>%
+  step_string2factor(gender, risk_class, face_amount, premium_mode) %>%
+  prep(strings_as_factors = FALSE)
+
+training_h2o <- as.h2o(bake(rec, training_data), "training_data")
+# training_h2o
 predictors <- c(
   "gender", "risk_class", "face_amount", "premium_mode", "avg_issue_age", "duration",
   "avg_premium_jump_ratio"
@@ -13,3 +19,15 @@ automl_model <- h2o.automl(
   stopping_metric = "RMSE",
   max_runtime_secs = 300
 )
+
+# check for reasonability
+
+predictions <- predict(automl_model, training_h2o) %>%
+  as.data.frame()
+
+training_data %>%
+  select(lapse_count_rate, exposure_count) %>%
+  cbind(predictions) %>%
+  weighted_rmse(truth = "lapse_count_rate", estimate = "predict", weights = "exposure_count")
+
+h2o.shutdown()
