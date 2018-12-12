aecb73d1f21afe77c9ccee92d2999dc2f3601169
kevinykuo <kevinykuo@gmail.com>
2018-12-12 18:37:05 -0500

    add weighted_rmse()

---
+++ R/utils.R
@@ -0,0 +1,15 @@
+#' Weighted RMSE
+#'
+#' Calculates weighted RMSE
+#'
+#' @param data The data frame with predicted and actual values.
+#' @param truth Column name for actual values.
+#' @param estimate Column name for predicted values.
+#' @param weights Column name for weights.
+#'
+#' @export
+weighted_rmse <- function(data, truth, estimate, weights) {
+  weighted_mse <- sum(data[[weights]] * (data[[truth]] - data[[estimate]]) ^ 2) /
+    sum(data[[weights]])
+  sqrt(weighted_mse)
+}
