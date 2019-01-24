d91b3c9d4f6ddf6681bd8fa290f8e570e0f26302
kevinykuo <kevinykuo@gmail.com>
2019-01-24 15:17:13 -0500

    don't aggregate factor levels, use embeddings

--- analysis/neuralnet.R
+++ analysis/neuralnet.R
@@ -5,7 +5,11 @@ source("analysis/data-prep.R")
 
 predictors <- c(
   "gender", "avg_issue_age", "face_amount",
-  "avg_premium_jump_ratio", "risk_class_mapped", "premium_mode", "duration"
+  "risk_class",
+  "premium_jump_ratio",
+  # "avg_premium_jump_ratio",
+  # "risk_class_mapped",
+  "premium_mode", "duration"
 )
 responses <- c("lapse_count_rate", "lapse_amount_rate")
 
@@ -19,9 +23,18 @@ f <- as.formula(paste(
 
 rec_nn <- recipe(f, data = training_data) %>%
   update_role(!!weights, new_role = "sample_weight") %>%
-  step_integer(duration, gender, risk_class_mapped, face_amount, premium_mode, zero_based = TRUE) %>%
-  step_center(avg_issue_age, avg_premium_jump_ratio) %>%
-  step_scale(avg_issue_age, avg_premium_jump_ratio) %>%
+  step_integer(
+    risk_class,
+    premium_jump_ratio,
+    duration,
+    gender,
+    # risk_class_mapped,
+    face_amount,
+    premium_mode,
+    zero_based = TRUE
+  ) %>%
+  step_center(avg_issue_age) %>% # , avg_premium_jump_ratio
+  step_scale(avg_issue_age) %>% # , avg_premium_jump_ratio
   prep(retain = TRUE, stringsAsFactors = FALSE)
 
 # Helper function to create a keras data model,
@@ -33,11 +46,11 @@ prep_keras_data <- function(data, predictors, responses) {
       "gender",
       ~ keras::to_categorical(.x, 2) %>% array_reshape(c(length(.x), 2))
     ) %>%
-    map_at(
-      "premium_mode",
-      ~ keras::to_categorical(.x, 6) %>%
-        array_reshape(c(length(.x), 6))
-    ) %>%
+    # map_at(
+    #   "premium_mode",
+    #   ~ keras::to_categorical(.x, 6) %>%
+    #     array_reshape(c(length(.x), 6))
+    # ) %>%
     map_at(
       "duration",
       ~ keras::to_categorical(.x, 3) %>%
@@ -59,26 +72,37 @@ make_keras_model <- function() {
   input_gender <- layer_input(shape = 2, name = "gender")
   input_issue_age_group <- layer_input(shape = 1, name = "avg_issue_age")
   input_face_amount_band <- layer_input(shape = 1, name = "face_amount")
-  input_avg_premium_jump_ratio <- layer_input(shape = 1, name = "avg_premium_jump_ratio")
-  input_risk_class <- layer_input(shape = 1, name = "risk_class_mapped")
-  input_premium_mode <- layer_input(shape = 6, name = "premium_mode")
+
+  # input_avg_premium_jump_ratio <- layer_input(shape = 1, name = "avg_premium_jump_ratio")
+  input_premium_jump_ratio <- layer_input(shape = 1, name = "premium_jump_ratio")
+  # input_risk_class_mapped <- layer_input(shape = 1, name = "risk_class_mapped")
+  input_risk_class <- layer_input(shape = 1, name = "risk_class")
+
+  input_premium_mode <- layer_input(shape = 1, name = "premium_mode")
   input_duration <- layer_input(shape = 3, name = "duration")
 
-  embedded_input_risk_class <- input_risk_class %>%
-    layer_embedding(9, 2) %>%
+  embedding_risk_class <- input_risk_class %>%
+    layer_embedding(9, 2, name = "embedding_risk_class") %>%
     layer_flatten()
-  embedded_input_face_amount_band <- input_face_amount_band %>%
-    layer_embedding(4, 2) %>%
+  embedding_premium_jump_ratio <- input_premium_jump_ratio %>%
+    layer_embedding(24, 2, name = "embedding_premium_jump_ratio") %>%
+    layer_flatten()
+  embedding_face_amount_band <- input_face_amount_band %>%
+    layer_embedding(4, 2, name = "embedding_face_amount_band") %>%
+    layer_flatten()
+  embedding_premium_mode <- input_premium_mode %>%
+    layer_embedding(6, 2, name = "embedding_premium_mode") %>%
     layer_flatten()
 
   concat_inputs <- layer_concatenate(list(
     input_duration,
     input_gender,
     input_issue_age_group,
-    embedded_input_face_amount_band,
-    input_avg_premium_jump_ratio,
-    embedded_input_risk_class,
-    input_premium_mode
+    embedding_face_amount_band,
+    # input_avg_premium_jump_ratio,
+    embedding_premium_jump_ratio,
+    embedding_risk_class,
+    embedding_premium_mode
   ))
 
   main_layer <- concat_inputs %>%
@@ -99,7 +123,8 @@ make_keras_model <- function() {
       input_gender,
       input_issue_age_group,
       input_face_amount_band,
-      input_avg_premium_jump_ratio,
+      # input_avg_premium_jump_ratio,
+      input_premium_jump_ratio,
       input_risk_class,
       input_premium_mode
     ),
@@ -141,4 +166,4 @@ validation_data_with_preds <- validation_data %>% bind_cols(
 validation_data_with_preds %>%
   weighted_rmse(truth = "lapse_count_rate", estimate = "predicted_count_rate", weights = "exposure_count")
 
-# [1] 0.1716038
+# [1] 0.1695261
