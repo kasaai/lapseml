8bbf2bad108db91d47b0fc778746690811e1c77f
kevinykuo <kevinykuo@gmail.com>
2019-01-25 18:47:04 -0500

    consolidate explanation plots

--- analysis/explain-nn.R
+++ analysis/explain-nn.R
@@ -25,3 +25,16 @@ plot(vi_nn)
 
 pdp_nn <- variable_response(explainer_nn, variable = "avg_issue_age")
 plot(pdp_nn)
+
+## embedding weights
+
+model$get_layer("embedding_premium_jump_ratio")$get_weights() %>%
+  as.data.frame() %>%
+  cbind(rec_nn$steps[[1]]$key$premium_jump_ratio) %>%
+  mutate(value = gsub("^[A-Z]\\.[ ]+", "", value)) %>%
+  ggplot(aes(x = X1, y = X2, color = integer)) +
+  geom_point() +
+  ggrepel::geom_label_repel(aes(label = value)) +
+  theme_bw() +
+  guides(color = FALSE) +
+  ggtitle("Embedding weights for premium jump ratio")
